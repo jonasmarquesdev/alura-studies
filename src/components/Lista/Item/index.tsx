b8caeb536c75d7336ce4db7c46ea1953d1f0e8ad
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2022-11-29 02:38:22 -0300

    Feature/Mostrando a tarefa selecionada

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -16,7 +16,7 @@ export default function item({
   console.log("Item atual:", { tarefa, tempo, selecionado, completado, id });
   return (
     <li
-      className={style.item}
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
       onClick={() =>
         selecionaTarefa({
           tarefa,
