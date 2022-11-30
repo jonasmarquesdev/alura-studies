71c9818b66baca1a9a2019f8129654f8e994323c
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2022-11-30 20:26:53 -0300

    Feature/Criando função de conversão de horas para minutos e segundos

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,15 +1,19 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/date";
 
 export function Cronometro() {
+  console.log('conversão: ', tempoParaSegundos('01:01:01'));
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
-      <Botao>Começar</Botao>
+      <Botao>
+        Começar
+      </Botao>
     </div>
   );
 }
