f023316851813734d088a21c8e6cdd90766a3e62
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2022-12-05 07:22:54 -0300

    Feature/Finalizando tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -15,9 +15,9 @@ export default function item({
 }: Props) {
   return (
     <li
-      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`}
       onClick={() =>
-        selecionaTarefa({
+        !completado && selecionaTarefa({
           tarefa,
           tempo,
           selecionado,
@@ -28,6 +28,12 @@ export default function item({
     >
       <h3> {tarefa} </h3>
       <span> {tempo} </span>
+      {completado && 
+        <span 
+          className={style.concluido}
+          aria-label="tarefa completada"
+        >
+        </span>}
     </li>
   );
 }
