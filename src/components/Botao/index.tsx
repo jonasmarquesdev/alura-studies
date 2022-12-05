460dc9be3be137b99080fa7fc7074789202811c9
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2022-12-05 07:30:58 -0300

    Hotfix/Refatorando Class Component Botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import style from "./Botao.module.scss";
 
-class Botao extends React.Component<{ 
-  children: any 
-  type?: "button" | "submit" | "reset" | undefined, // a ? define que é uma props opcional
-  onClick?: () => void
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button onClick={onClick} type={type} className={style.botao}>
-        {this.props.children}
-      </button>
-    )
-  }
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void,
+  children?: React.ReactNode
 }
+
+function Botao({ onClick, type, children }: Props) {
+  return (
+    <button 
+      onClick={onClick} 
+      type={type} 
+      className={style.botao}
+    >
+      {children}
+    </button>
+  )
+}
+
+// class Botao1 extends React.Component<{ 
+//   children: any 
+//   type?: "button" | "submit" | "reset" | undefined, // a ? define que é uma props opcional
+//   onClick?: () => void
+// }> {
+//   render() {
+//     const { type = "button", onClick } = this.props;
+//     return (
+//       <button onClick={onClick} type={type} className={style.botao}>
+//         {this.props.children}
+//       </button>
+//     )
+//   }
+// }
 
 export default Botao;
